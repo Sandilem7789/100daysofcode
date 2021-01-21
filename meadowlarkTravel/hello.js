20d48250f6cf47f83d09f9779833ea33840f6325
sandilem7789 <sandilem7789@gmail.com>
2021-01-21 14:39:29 +0200

    Routing with node

--- meadowlarkTravel/hello.js
+++ meadowlarkTravel/hello.js
@@ -2,8 +2,23 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World");
+    //normalize url by removing querystring, optional
+    //trailing slash, and making it lower case
+    const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+    switch (path) {
+        case "":
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("HomePage");
+            break;
+        case "/about":
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end("About");
+            break;
+        default:
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("About");
+            break;
+    }
 });
 
 server.listen(port, () => console.log(`Server started on port ${port}: ` +
