a6be023de8f56639ca996fd74915bb55b3fd9b78
sandilem7789 <sandilem7789@gmail.com>
2021-01-22 16:47:34 +0200

    Handlebars view engine

---
+++ meadowlarkTravel/site/meadowlark.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const expressHandlebars = require("express-handlebars")
+
+const app = express();
+
+//configuring the Handlebars view engine
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+const port = process.env.PORT || 3000;
+
+//main page
+app.get("/", (req, res) => {
+    res.type("text/plain");
+    res.send("Meadowlark Travel");
+});
+
+app.get("/about", (req, res) => {
+    res.type("text/plain");
+    res.send("About Meadowlark Travel");
+});
+
+//custom 404 page
+app.use((req, res) => {
+    res.type("text/plain");
+    res.status(404);
+    res.send("404 - Not Found")
+});
+
+//custom 500 page
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.type("text/plain");
+    res.status(500);
+    res.send("500 - Server Error");
+});
+
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}: ` +
+    "press Ctrl-C to terminate"
+));
