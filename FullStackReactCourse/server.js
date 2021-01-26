ab8337c487418f8abf108c226d9e8d5c05141712
sandilem7789 <sandilem7789@gmail.com>
2021-01-26 18:48:36 +0200

    package-lock

---
+++ FullStackReactCourse/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+app.use(express.static("ch2_bootstrap"));
+
+app.listen(3000, () => {
+    console.log("App started on port 3000, press Ctrl-C to terminate..");
+});
