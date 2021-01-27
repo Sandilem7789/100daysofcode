668d7ed9df56830bb2f2dbc20bbd7e8f7b83b5a1
sandilem7789 <sandilem7789@gmail.com>
2021-01-27 03:55:39 +0200

    Initial Settup

--- FullStackReactCourse/server.js
+++ FullStackReactCourse/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-app.use(express.static("ch2_bootstrap"));
+app.use(express.static("./ch2_bootstrap"));
 
 app.listen(3000, () => {
     console.log("App started on port 3000, press Ctrl-C to terminate..");
