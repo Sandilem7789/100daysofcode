47afa3c8d087d604342c368c2e1eea4dcd228f9e
sandilem7789 <sandilem7789@gmail.com>
2021-02-24 22:22:19 +0200

    configuring a react application

--- FullStackReactCourse/React/confusion/src/App.js
+++ FullStackReactCourse/React/confusion/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+//import logo from './logo.svg';
+import {Navbar, NavbarBrand} from "reactstrap";                              //boostrap component 
 import './App.css';
 
-let App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">
+                Ristorante Con Funsion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
