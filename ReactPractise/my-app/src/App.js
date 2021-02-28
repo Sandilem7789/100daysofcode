05b7c52580e338bdc515fd2c40186f4bf3226d0a
sandilem7789 <sandilem7789@gmail.com>
2021-02-28 10:27:16 +0200

    react toggle

--- ReactPractise/my-app/src/App.js
+++ ReactPractise/my-app/src/App.js
@@ -7,20 +7,28 @@ const date = new Date();
 class App extends Component {
    constructor(props){
 		super(props);
+		this.state = {isToggleOn: true};
 	}
+
+	handleClick = () => {
+		this.setState(state => ({
+			isToggleOn: !state.isToggleOn
+		}));
+	}
+
 	render(){
 		return (
 			<div className="App">
-			  <header className="App-header">
-				 <img src={logo} className="App-logo" alt="logo" />
-				 <p>
-					It is {date.toLocaleTimeString()} 
-				 </p>
-				 <a href="#" onclick="console.log('The link was clicked')">
-
-				 </a>
-		  </header>
-		</div>
+			  	<header className="App-header">
+				 	<img src={logo} className="App-logo" alt="logo" />
+				 	<p>
+						It is not {date.toLocaleTimeString()} to the dot 
+				 	</p>
+				 	<button onClick={this.handleClick}>
+						{this.state.isToggleOn ? "ON" : "OFF"}
+				 	</button>
+			  	</header>
+			</div>
 	 	);
   	}	
 }
