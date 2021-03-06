c98afdf73d898fcdeedb2ba6aef45217dbe565fd
sandilem7789 <sandilem7789@gmail.com>
2021-03-06 14:01:55 +0200

    further db.json

--- ReactPractise/my-app/src/App.js
+++ ReactPractise/my-app/src/App.js
@@ -1,33 +1,70 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-import { useState } from 'react';
+import AddTask from './components/AddTask';
+import { useState, useEffect } from 'react';
 
 const App = () => {
-	const [tasks, setTasts] = useState([
-		{
-			id: 1,
-			text: 'Doctors Appointment',
-			day: 'Feb 5th at 2:30pm',
-			reminder: true
-		},
-		{
-			id: 2,
-			text: 'Meeting At School',
-			day: 'Feb 6th at 1:30pm',
-			reminder: true
-		},
-		{
-			id: 3,
-			text: 'Food Shopping',
-			day: 'Feb 6th at 1:30pm',
-			reminder: false
-		},
-  ]);
+	const [showAddTask, setShowAddTask] = useState(false)
+	const [tasks, setTasks] = useState([
+		
+  	]);
+
+
+	/* UseEffect: this methods is for side effect*/
+	useEffect(() => {
+		const getTasks = async () => {
+			const tasksFromServer = await fetchTasks();
+			setTasks(tasksFromServer)
+		}
+		getTasks();
+	}, []);
+
+	/*****Fetch Tasks From the backend: db.json*****/
+	const fetchTasks = async () => {
+		const res = await fetch("http://localhost:5000/tasks");
+		const data = await res.json();
+		return data;
+	}
+
+	/******Add Task******/
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 10000) + 1;
+		const newTask = { id, ...task };
+		setTasks([...tasks, newTask]);
+	}
+
+  	/****DELETE TASK FUNCTION****/
+  	const deleteTask = (id) => {
+		setTasks(tasks.filter((task) => task.id !== id));
+  	}
+   
+	/****STYLING COMPONENT*****/
+	const style = {
+		textAlign: "center",
+		fontWeight: "lighter"
+	};
+
+	/*******TOGGLE REMINDER******/
+	const toggleReminder = (id) => {
+		setTasks(
+			tasks.map((task) =>
+				task.id === id ? 
+				{...task, reminder: !task.reminder} : task
+			)
+		);
+	};
 
 	return(
 		<div className="container">
-			<Header/>
-			<Tasks  tasks={tasks}/>
+			<Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+			{/*TERNARY WITH NO ELSE STATEMENT*/}
+			{showAddTask && <AddTask onAdd={addTask}/>}
+
+			{
+				tasks.length > 0 ? 
+				(<Tasks  tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : 
+				(<h3 style={style}>No Tasks To Show</h3>)
+			}
 		</div>
 	);
 }
--- ReactPractise/my-app/src/components/Task.js
+++ ReactPractise/my-app/src/components/Task.js
@@ -1,10 +1,15 @@
 import { FaTimes } from "react-icons/fa";
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete, onToggle }) => {
+	const style = {
+		color: "red",
+		cursor: "pointer",
+	}
+	
    return (
-      <div className='task'>
+      <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
 			<h3>
-				{task.text} <FaTimes />
+				{task.text} <FaTimes style={style} onClick={() => onDelete(task.id)}/>
 			</h3>
 			<p>{task.day}</p>
       </div>
