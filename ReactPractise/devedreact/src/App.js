7fc426a74b9f7fa28cf237a0efc59897b255ddf1
sandilem7789 <sandilem7789@gmail.com>
2021-03-08 16:20:34 +0200

    cleaned

--- ReactPractise/devedreact/src/App.js
+++ ReactPractise/devedreact/src/App.js
@@ -1,22 +1,10 @@
 import React, { useState } from "react";
-import Tweet from "./components/Tweet";
 
 function App(){
-	const [users, setUsers] = useState([
-		{name: "Ed", message: "Hello there John Snow.", likes: "2k"},
-		{name: "John", message: "Who is John Snow now??", likes: "4k"},
-		{name: "Traversy", message: "Quit it John! this is Ed, Dev Ed!", likes: "6k"}
-	]);
-	
-   	return(
+
+	return(
 		<div className="app">
-			{users.map(user => (
-				<Tweet 
-					name={user.name}
-					message={user.message}
-					likes={user.likes}
-				/>
-			))}
+
 		</div>
 	);
 }
