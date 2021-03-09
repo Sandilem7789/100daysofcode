c79c721fd1c5be8c46962acd0dbf5d41a7a5054a
sandilem7789 <sandilem7789@gmail.com>
2021-03-09 12:22:00 +0200

    yarn in the mix now

---
+++ ReactPractise/brian-design-tut/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from "@material-ui/core/Button";
+import SaveIcon from "@material-ui/icons/Save";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button
+          startIcon={<SaveIcon />}
+          size="large"
+          variant="outlined" 
+          color="secondary"
+        >
+          Hello Material
+        </Button>
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
