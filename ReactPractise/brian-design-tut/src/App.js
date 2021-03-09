e0c847221070cc6bba03fc321fa1f5a55561d1cf
sandilem7789 <sandilem7789@gmail.com>
2021-03-09 18:38:01 +0200

    toggling icons on the checkbox

--- ReactPractise/brian-design-tut/src/App.js
+++ ReactPractise/brian-design-tut/src/App.js
@@ -1,17 +1,37 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
 import DeleteIcon from "@material-ui/icons/Delete";
-import Checkbox from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/CheckBox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
-
+const CheckBoxExample = () => {
+  const [checked, setChecked] = React.useState(true)
+  return(
+    <FormControlLabel
+      control={
+        <Checkbox
+        checked={checked}
+        icon={<DeleteIcon color="secondary" />}
+        checkedIcon={<SaveIcon />}
+        onChange={(e) => setChecked(e.target.checked)}
+        inputProps={{
+          "aria-label": "secondary checkbox"
+        }}
+      />}
+      label="Testing CheckBox"
+    />
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CheckBoxExample />
         <ButtonGroup
           size="large"
           variant="contained" 
