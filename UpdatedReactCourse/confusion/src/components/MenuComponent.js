13d76ec6b24aeccd0483b7911b17fbd5f9dc0606
sandilem7789 <sandilem7789@gmail.com>
2021-03-11 16:29:02 +0200

    components part 2

--- UpdatedReactCourse/confusion/src/components/MenuComponent.js
+++ UpdatedReactCourse/confusion/src/components/MenuComponent.js
@@ -14,22 +14,49 @@ class Menu extends React.Component {
         super(props);
 
         this.state = {
-            
+            selectedDish: null
         }
     }
+
+    onDishSelect (dish) {
+        this.setState({selectedDish: dish});
+    }
+
+    renderDish(dish) {
+        if(dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>
+                            {dish.name}
+                        </CardTitle>
+                        <CardText>
+                            {dish.description}
+                        </CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+
     render(){
         const menu = this.props.dishes.map((dish) => {
             return(
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card tag="li">
+                    <Card tag="li" onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay body className="ml-5">
-                            <CardTitle tag="h1">
+                            <CardTitle tag="h2">
                                 {dish.name}
                             </CardTitle>
-                            <p>
-                                {dish.description}
-                            </p>
+                            
                         </CardImgOverlay>
                     </Card>
                 </div>
@@ -39,6 +66,9 @@ class Menu extends React.Component {
             <div className="container">
                 <div className="row">
                     {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         )
