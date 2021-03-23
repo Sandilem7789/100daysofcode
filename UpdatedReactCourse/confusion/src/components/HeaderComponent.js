4247a2d326a6b86c351b8f8c047de96ab4ebe68b
sandilem7789 <sandilem7789@gmail.com>
2021-03-23 17:22:41 +0200

    uncontrolled forms

--- UpdatedReactCourse/confusion/src/components/HeaderComponent.js
+++ UpdatedReactCourse/confusion/src/components/HeaderComponent.js
@@ -3,7 +3,11 @@ import {
     Navbar, NavbarBrand,
     Nav, NavbarToggler,
     Collapse, NavItem,
-    Jumbotron
+    Jumbotron, Button,
+    Modal, ModalHeader,
+    ModalBody, Form,
+    FormGroup, Input,
+    Label
 } from 'reactstrap';
 import { NavLink } from "react-router-dom";
 
@@ -11,9 +15,12 @@ class Header extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isNavOpen: false
+            isNavOpen: false,
+            isModalOpen: false
         }
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
     toggleNav() {
@@ -22,9 +29,28 @@ class Header extends React.Component {
         });
     }
 
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleLogin(e) {
+        this.toggleModal();
+        alert(`
+            Username: ${this.username.value} 
+            Password: ${this.password.value} 
+            Remember me: ${this.remember.checked}
+            form submitted`
+        );
+        
+        e.preventDafault();
+        
+    }
+
     render() {
         return(
-            <>
+            <React.Fragment>
                 <Navbar dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
@@ -59,6 +85,14 @@ class Header extends React.Component {
                                     </NavLink>
                                 </NavItem>
                             </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem onClick={this.toggleNav}>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-sign-in fa-lg"></span>{" "}
+                                        Login
+                                    </Button>
+                                </NavItem>
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
@@ -79,7 +113,31 @@ class Header extends React.Component {
                         </div>
                     </div>
                 </Jumbotron>
-            </>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>
+                        Login
+                    </ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username" innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password" innerRef={(input) => this.password = input} />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember" innerRef={(input) => this.remember = input} />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
         );
     }
 }
