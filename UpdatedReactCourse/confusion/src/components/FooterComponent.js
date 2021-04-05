cb4019d46ad2400500fbb3bdc3615f511fc73a52
sandilem7789 <sandilem7789@gmail.com>
2021-04-05 09:22:42 +0200

    d65edits

--- UpdatedReactCourse/confusion/src/components/FooterComponent.js
+++ UpdatedReactCourse/confusion/src/components/FooterComponent.js
@@ -18,9 +18,9 @@ const Footer = (props) => {
                     <div className="col-7 col-sm-5">
                         <h5>Our Address</h5>
                         <address>
-                        121, Clear Water Bay Road<br />
-                        Clear Water Bay, Kowloon<br />
-                        HONG KONG<br />
+                        121, Tojiwe Village<br />
+                        KwaNtaba,<br />
+                        Mhlekazi<br />
                         <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
                         <i className="fa fa-fax fa-lg"></i>: +852 8765 4321<br />
                         <i className="fa fa-envelope fa-lg"></i>: <a href="mailto:confusion@food.net">
