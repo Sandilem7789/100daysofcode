a099fbd6ae9b1505c654a508995e7d011d5df3c4
sandilem7789 <sandilem7789@gmail.com>
2021-07-07 15:13:40 +0200

    app bar

--- ReactPractise/my-app/src/App.js
+++ ReactPractise/my-app/src/App.js
@@ -6,11 +6,79 @@ import AddTask from './components/AddTask';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { useState, useEffect } from 'react';
 
+/*
+		ADDING MATERIAL UI
+*/
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { alpha, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+	  flexGrow: 1,
+	},
+	menuButton: {
+	  marginRight: theme.spacing(2),
+	},
+	title: {
+	  flexGrow: 1,
+	  display: 'none',
+	  [theme.breakpoints.up('sm')]: {
+		display: 'block',
+	  },
+	},
+	search: {
+	  position: 'relative',
+	  borderRadius: theme.shape.borderRadius,
+	  backgroundColor: alpha(theme.palette.common.white, 0.15),
+	  '&:hover': {
+		backgroundColor: alpha(theme.palette.common.white, 0.25),
+	  },
+	  marginLeft: 0,
+	  width: '100%',
+	  [theme.breakpoints.up('sm')]: {
+		marginLeft: theme.spacing(1),
+		width: 'auto',
+	  },
+	},
+	searchIcon: {
+	  padding: theme.spacing(0, 2),
+	  height: '100%',
+	  position: 'absolute',
+	  pointerEvents: 'none',
+	  display: 'flex',
+	  alignItems: 'center',
+	  justifyContent: 'center',
+	},
+	inputRoot: {
+	  color: 'inherit',
+	},
+	inputInput: {
+	  padding: theme.spacing(1, 1, 1, 0),
+	  // vertical padding + font size from searchIcon
+	  paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+	  transition: theme.transitions.create('width'),
+	  width: '100%',
+	  [theme.breakpoints.up('sm')]: {
+		width: '12ch',
+		'&:focus': {
+		  width: '20ch',
+		},
+	  },
+	},
+  }));
+  
+
 const App = () => {
 	const [showAddTask, setShowAddTask] = useState(false)
-	const [tasks, setTasks] = useState([
-		
-  	]);
+	const [tasks, setTasks] = useState([]);
+
+	const classes = useStyles();
 
 
 	/* UseEffect: this methods is for side effect*/
@@ -99,7 +167,38 @@ const App = () => {
 
 	return(
 		<Router>
+			<div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Task Tracker
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
 			<div className="container">
+
 				<Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
 				
 				<Route path="/" exact render={(props) => (
